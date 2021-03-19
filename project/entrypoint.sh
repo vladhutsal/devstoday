ed9b747c98430e1b01ee6dc3605e8a0bb97c9630
vlhutsal <vl.hutsal@gmail.com>
2021-03-19 14:59:19 +0200

    Heroku.yml update

--- project/entrypoint.sh
+++ project/entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-echo " -------------- waiting for postgres to start"
-while ! nc -z db 5432; do
-    sleep 0.1
-done
-
-echo " -------------- postgres started"
-
 python3 manage.py makemigrations
 python3 manage.py migrate
 
